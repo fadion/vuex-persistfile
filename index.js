cd65c9ef20ca3722efa04d62e24121ca4c533d32
Fadion Dashi <jonidashi@gmail.com>
2018-01-27 16:03:31 +0100

    Add index.js file
    Signed-off-by: Fadion Dashi <jonidashi@gmail.com>

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Persist Vuex state in the filesystem for Electron.
+ *
+ * @module main
+ * @author Fadion Dashi
+ * @license MIT
+ */
+
+const VuexPersist = require('./lib/vuex-persistfile')
+
+module.exports = VuexPersist
